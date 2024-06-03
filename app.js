85143e225502c22e51571f14f85ce775885a52df
bhavesh-buwa <bhaveshbuwa7@gmail.com>
2024-06-03 08:24:23 +0530

    part 01

---
+++ app.js
@@ -0,0 +1,15 @@
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading1" }, "This is heading one"),
+    React.createElement("h1", { id: "heading1" }, "This is heading one"),
+  ])
+);
+
+// prints the heading object
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
